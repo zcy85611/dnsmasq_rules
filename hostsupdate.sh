03ad584e44596819448edaa50a2f22eda6551d3d
skygunner5 <skygunner5@hotmail.com>
2023-01-18 04:33:35 -0500

    Wed 18 Jan 2023 04:33:35 AM EST

--- hostsupdate.sh
+++ hostsupdate.sh
@@ -3,6 +3,7 @@ curl https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt | sed "/
 curl https://raw.githubusercontent.com/Goooler/1024_hosts/master/hosts | sed 's/\r//g' | awk '{print $2}' > 2222
 curl https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | grep ^\|\|[^\*]*\^$ | sed -e 's/||//g' |sed -e 's/\^//g' > 3333
 curl https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt | grep ^\|\|[^\*]*\^$ | sed -e 's/||//g' -e 's/\^//g' > 4444
-cat 1111 2222 3333 4444 > 5555
-cat 5555|sort|sed -e '/host/d'|awk '!a[$0]++'|awk '{print "0.0.0.0 " $0}' > hosts_AD
-rm 1111 2222 3333 4444 5555
+curl https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt|tail -n +25 | sed "/#/d" | sed '/^s*$/d' | awk '{print $2}' > 5555
+cat 1111 2222 3333 4444 5555 > 6666
+cat 6666|sort|sed -e '/host/d'|awk '!a[$0]++'|awk '{print "0.0.0.0 " $0}' > hosts_AD
+rm 1111 2222 3333 4444 5555 6666
