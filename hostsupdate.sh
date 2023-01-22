ab78fbdd47a45a0b95326e628a75e47152874292
skygunner5 <skygunner5@hotmail.com>
2023-01-22 00:00:13 -0500

    Sun 22 Jan 2023 12:00:12 AM EST

--- hostsupdate.sh
+++ hostsupdate.sh
@@ -5,5 +5,10 @@ curl https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt | gre
 curl https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt | grep ^\|\|[^\*]*\^$ | sed -e 's/||//g' -e 's/\^//g' > 4444
 curl https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt|tail -n +25 | sed "/#/d" | sed '/^s*$/d' | awk '{print $2}' > 5555
 cat 1111 2222 3333 4444 5555 > 6666
+sed -i "/down.sandai.net/d" 6666
+sed -i "/jf365.boc.cn/d" 6666
+sed -i "/as.boc.cn/d" 6666
+sed -i "/fex.bdstatic.com/d" 6666
+sed -i "/t.uc.cn/d" 6666
 cat 6666|sort|sed -e '/host/d'|awk '!a[$0]++'|awk '{print "0.0.0.0 " $0}' > hosts_AD
 rm 1111 2222 3333 4444 5555 6666
