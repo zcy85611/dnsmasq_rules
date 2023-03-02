809b7c4e68b1bf483cfc0a939e20f95ed1e75e61
skygunner5 <skygunner5@hotmail.com>
2023-03-02 08:33:34 +0800

    Thu 02 Mar 2023 08:33:34 AM CST

--- hostsupdate.sh
+++ hostsupdate.sh
@@ -19,5 +19,7 @@ sed -i "/ws.mmstat.com/d" 6666
 sed -i "/asearch.alicdn.com/d" 6666
 sed -i "/wgo.mmstat.com/d" 6666
 sed -i "/t.uc.cn/d" 6666
+sed -i "/oth.eve.mdt.qq.com/d" 6666
+sed -i "/android.bugly.qq.com/d" 6666
 cat 6666|sort|sed -e '/host/d'|awk '!a[$0]++'|awk '{print "0.0.0.0 " $0}' > hosts_AD
 rm 1111 2222 3333 4444 5555 6666
